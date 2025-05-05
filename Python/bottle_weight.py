1e337962f143bcf0c10c72c6cb23c4d578b982df
Ryan Costa <89921782+ryancosta15@users.noreply.github.com>
2025-05-05 19:13:02 -0300

    Update bottle_weight.py

--- Python/bottle_weight.py
+++ Python/bottle_weight.py
@@ -2,4 +2,4 @@ def content_weight(bottle_weight, scale):
     scale = scale.split(" ")
     times = int(scale[0])
     part = bottle_weight / (times+1)
-    return part * times if scale[2] == "larger" else part
+    return part if scale[2] == "smaller" else part * times
